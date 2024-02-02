77be2c33e446cf8c8c52cd2c0fdad093f8f0fa71
amyscr <143654611+amyscr@users.noreply.github.com>
2024-02-02 01:48:37 -0500

    Update amyscr_HWx_Q1.cpp

--- amyscr_HWx_Q1.cpp
+++ amyscr_HWx_Q1.cpp
@@ -1,36 +1,58 @@
 #include <iostream>
-
 using namespace std;
 
 struct Check
 {
-    int checkNum;
-    string checkMemo;
-    float checkAmount;
+    int checkNum = 1;
+    string checkMemo = "";
+    float checkAmount = 0.0;
 };
+
+const int N = 5;
 
 class CheckBook{
 public:
     CheckBook(): balance(0), numOfChecks(0){} //default constructor
     CheckBook(float initBalance): balance(initBalance), numOfChecks(0){}
 
-    const int n = 5;
-    Check checks[n];
+    Check checks[N];
 
-    void deposit(float amount){balance=balance+amount;}
-    void DisplayChecks(Check checks);
+    void deposit(float amount){balance=balance+amount; numOfChecks++;}
+    void DisplayChecks();
 
-    bool writeCheck(float amount);
+    bool writeCheck(float amount); //basically a setter fcn
 private:
-    float balance; //balance to keep track of balance
+    float balance; //keep track of balance
     float lastDeposit; //last deposited amt
     int numOfChecks;
-    int checkLimit = n;
+    int checkLimit = N;
 };
+
+bool CheckBook::writeCheck(float amount)
+{
+    //ask
+    cout<<"Check amount? "; cin>>Check.checkAmount;
+    cout<<"\nCheck Memo? "; cin>>Check.checkMemo;
+    //set checknum with numofchecks from checkbook
+    //update balance
+    balance = balance + Check.checkAmount;
+    //update numofchecks
+    if(balance>amount)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 
 void CheckBook::DisplayChecks()
 {
-
+    for(int i=0; i<numOfChecks; i++)
+    {
+        cout<<Check.checkNum;
+    }
 }
 
 int main()
@@ -38,4 +60,6 @@ int main()
     cout << "Hello world!" << endl;
     return 0;
     CheckBook check1, check2(2);
+
+    //CheckBook check3(deposit());
 }
